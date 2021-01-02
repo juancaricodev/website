45ed7b8e73c9149ec0268ff4703ad0ceebce334d
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-02 04:02:48 -0500

    feat: connection to firestore made and message saving implemented

--- js/main.js
+++ js/main.js
@@ -10,22 +10,44 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
+// Connection to Firestore
+const db = firebase.firestore()
+
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm)
 
+// Submit form
 function submitForm(e) {
   e.preventDefault()
 
+  // Get values
   const name = getInputVal('name')
-  const lastname = getInputVal('lastname')
   const company = getInputVal('company')
   const email = getInputVal('email')
   const message = getInputVal('message')
 
-  console.log(name)
+  // Save message
+  saveMessage(name, company, email, message)
 }
 
 // Function to get form values
 function getInputVal(id) {
   return document.getElementById(id).value
+}
+
+// Save message to Firestore
+function saveMessage(name, company, email, message) {
+  return db.collection('messages').add({
+    name: name,
+    company: company,
+    email: email,
+    message: message,
+    date: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(() => {
+    console.log(`Message saved`)
+  })
+  .catch(error => {
+    console.error(`Error saving message => ${error}`)
+  })
 }
