7491fd49f94b3014597259a7532203cb8085ce0b
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-02 02:36:10 -0500

    feat: firebase project "contact-form" created and config added to main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBgUHFu7CWjIycRgMY5YMqV1aZLP8GBhOM",
+    authDomain: "contact-form-eb6be.firebaseapp.com",
+    projectId: "contact-form-eb6be",
+    storageBucket: "contact-form-eb6be.appspot.com",
+    messagingSenderId: "665877994355",
+    appId: "1:665877994355:web:5c425f5c9ca7da887f8306"
+  }
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm)
 
 function submitForm(e) {
   e.preventDefault()
 
-  console.log(123)
+  const name = getInputVal('name')
+  const lastname = getInputVal('lastname')
+  const company = getInputVal('company')
+  const email = getInputVal('email')
+  const message = getInputVal('message')
+
+  console.log(name)
+}
+
+// Function to get form values
+function getInputVal(id) {
+  return document.getElementById(id).value
 }
